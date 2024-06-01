faa0589029c820de9d36000b467e0c945a35fa72
wangshufen <15295502569@163.com>
2024-06-02 01:06:11 +0800

    fix: modify login

--- auth.config.ts
+++ auth.config.ts
@@ -6,21 +6,22 @@ export const authConfig = {
   },
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
-      // console.log('nextUrl: ', nextUrl);
+      console.log('nextUrl: ', nextUrl);
       const isLoggedIn = !!auth?.user;
-
-      // if (isOnDashboard) {
-      //   if (isLoggedIn) return true;
-      //   return false; // Redirect unauthenticated users to login page
-      // } else if (isLoggedIn) {
-      //   return Response.redirect(new URL('/dashboard', nextUrl));
-      // }
-      // return true;
+      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      if (isOnDashboard) {
+        if (isLoggedIn) return true;
+        return false; // Redirect unauthenticated users to login page
+      } else if (isLoggedIn) {
+        // return Response.redirect(new URL('/dashboard', nextUrl));
+        return true;
+      }
+      return true;
       // console.log('isLoggedIn: ', auth);
-      if (isLoggedIn && nextUrl.pathname == '/') {
-        return Response.redirect(new URL('/dashboard', nextUrl));
-      } else if (isLoggedIn) return true;
-      return false; // Redirect unauthenticated users to login page
+      // if (isLoggedIn && nextUrl.pathname == '/') {
+      //   return Response.redirect(new URL('/dashboard', nextUrl));
+      // } else if (isLoggedIn) return true;
+      // return false; // Redirect unauthenticated users to login page
     }
   },
   providers: [] // Add providers with an empty array for now
